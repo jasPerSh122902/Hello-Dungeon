8ea3bde96e5b03c447ed15896e66f2029da5366f
jasPerSh122902 <88592760+jasPerSh122902@users.noreply.github.com>
2021-08-20 00:15:50 -0500

    i finaly finished meny ideas.

--- Hello Dungeon/Game.cs
+++ Hello Dungeon/Game.cs
@@ -10,44 +10,47 @@ namespace Hello_Dungeon
         {
 
             //Health and Health Regon
-            int healthRegon = 0;
-            int health  =  15;
-            int speed = 10;
-            int intellignece = 6;
-            int stamana = 4;
-
-            //damage
-            int damagePhysical = 5;
-                {
-                bool (damagePhysical = true) ;
-                }
-            Console.WriteLine("You have dealt 1 physical damage");
-            Console.WriteLine("You have dealt 2 magical damage");
-            int damageMagical = 2;
-
-            //increase health by Regon value
-            health += healthRegon;
-
-            //Health shown to player
-
-            Console.WriteLine("Health " + health);
-            Console.WriteLine("Damage Physical " + damagePhysical);
-            Console.WriteLine("Damage Magacial " + damageMagical);
-            Console.WriteLine("Intelligence " + intellignece);
-            Console.WriteLine("Stamana " + stamana);
-
-            //Name system - this is going to be the intro
             string name = "Empty";
+            int healthRegon = 0;
+            int intellignece = 6;
+
+            Console.WriteLine("1.Knight");
+            Console.WriteLine("2.Wizard");
+            Console.ReadLine();
+
+
+            while
+            {
+                if name == "1";
+                {
+                    //the two caractors that are in the game
+                    int health = 15;
+                    int damage = 3;
+                    int stamana = 10;
+                    Console.WriteLine("Health" + health);
+                    Console.WriteLine("Damage" + damage);
+                    Console.WriteLine("stamana" + stamana);
+                }
+
+                else Console.ReadLine()
+                {
+                    int health = 7;
+                    int damage = 25;
+                    int stamana = 4;
+                    Console.WriteLine("Health" + health);
+                    Console.WriteLine("Damage" + damage);
+                    Console.WriteLine("stamana" + stamana);
+
+                }
+
+            }
+
+          //the intro for the added caractor creation
             Console.WriteLine("Travaler you have come a long way to just up and loose your life. If you have the will for adventure let me hear your name.");
             name = Console.ReadLine();
             Console.WriteLine(name + " What a splendid travaler you are to come so far and then this.");
             Console.WriteLine("Now the deal is made and there is no going back.");
             Console.WriteLine("So were do we travel first partner");
-
-
-            //Damage wompus bompus
-
-            
         }
     }
 }
